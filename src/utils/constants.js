dcc13a7da22431907893f758eece0176c2d7872c
ifeola <nomorelimit11@gmail.com>
2025-04-13 09:36:15 +0100

    - Corrected an error

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,83 +1,83 @@
 import {
-  CSS3,
-  GIT,
-  GITHUB,
-  HTML5,
-  JavaScript,
-  LinkedIn,
-  React,
-  TailwindCSS,
-  Twitter,
-  TypeScript,
+	CSS3,
+	GIT,
+	GITHUB,
+	HTML5,
+	JavaScript,
+	LinkedIn,
+	React,
+	TailwindCSS,
+	Twitter,
+	TypeScript,
 } from "../components/SkillSVG";
 
 export const navLinks = ["Home", "About Me", "Projects", "Skills", "Contact"];
 
 export const stackList = [
-  "HTML",
-  "CSS",
-  "JavaScript (ES6+)",
-  "TailwindCSS",
-  "React",
-  "TypeScript",
-  "Git",
-  "Github",
+	"HTML",
+	"CSS",
+	"JavaScript (ES6+)",
+	"TailwindCSS",
+	"React",
+	"TypeScript",
+	"Git",
+	"Github",
 ];
 
 export const HERO_COPY =
-  "I am a motivated and enthusiastic web developer looking to kick-start my career. I am dedicated to building innovative and user-friendly solutions that make a difference. Explore my portfolio to see my projects and learn more about my journey as a web developer.";
+	"I am a motivated and enthusiastic web developer looking to kick-start my career. I am dedicated to building innovative and user-friendly solutions that make a difference. Explore my portfolio to see my projects and learn more about my journey as a web developer.";
 
 export const socialLinks = [
-  {
-    content: "Github",
-    link: "https://github.com/ifeola",
-    icon: GITHUB,
-  },
-  {
-    content: "LinkedIn",
-    link: "https://www.linkedin.com/in/abayomi-arogunmasa-75254a134/",
-    icon: LinkedIn,
-  },
-  {
-    content: "X(Twitter)",
-    link: "https://x.com/abayomi_codes",
-    icon: Twitter,
-  },
+	{
+		content: "Github",
+		link: "https://github.com/ifeola",
+		icon: GITHUB,
+	},
+	{
+		content: "LinkedIn",
+		link: "https://www.linkedin.com/in/abayomi-arogunmasa-75254a134/",
+		icon: LinkedIn,
+	},
+	{
+		content: "X(Twitter)",
+		link: "https://x.com/abayomi_codes",
+		icon: Twitter,
+	},
 ];
 
 export const skills = [
-  {
-    skill: "HTML5",
-    svg: HTML5,
-  },
-  {
-    skill: "CSS3",
-    svg: CSS3,
-  },
-  {
-    skill: "JavScript(ESC+)",
-    svg: JavaScript,
-  },
-  {
-    skill: "TypeScript",
-    svg: TypeScript,
-  },
-  {
-    skill: "React",
-    svg: React,
-  },
-  {
-    skill: "TailwindCSS",
-    svg: TailwindCSS,
-  },
-  {
-    skill: "Git",
-    svg: GIT,
-  },
-  {
-    skill: "GitHub",
-    svg: GITHUB,
-  },
+	{
+		skill: "HTML5",
+		svg: HTML5,
+	},
+	{
+		skill: "CSS3",
+		svg: CSS3,
+	},
+	{
+		skill: "JavScript(ES6+)",
+		svg: JavaScript,
+	},
+	{
+		skill: "TypeScript",
+		svg: TypeScript,
+	},
+	{
+		skill: "React",
+		svg: React,
+	},
+	{
+		skill: "TailwindCSS",
+		svg: TailwindCSS,
+	},
+	{
+		skill: "Git",
+		svg: GIT,
+	},
+	{
+		skill: "GitHub",
+		svg: GITHUB,
+	},
 ];
 
 export const CONTACT_COPY = `I'm excited to connect with potential employers, mentors, and fellow
